d0bbf557fe0f897ce07eb6625e1bdf4e5f81f961
SmirnovDD <studioindevelopment@gmail.com>
2019-12-02 17:46:09 +0300

    sword

--- Assets/Scripts/CharacterMotor.cs
+++ Assets/Scripts/CharacterMotor.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class CharacterMotor : MonoBehaviour
 {
+    public Animator modelAnim;
+    public GameObject stringy;
+    public Transform headTr;
+
     public float maxVelocity;
     public float force;
     public Rigidbody rigidbodyMover;
@@ -14,25 +18,82 @@ public class CharacterMotor : MonoBehaviour
     private Rigidbody testRb;
     private Vector3 movementVector;
     private Transform cameraT;
+
+    private bool falling;
     private void Start()
     {
         testRb = GetComponent<Rigidbody>();
         cameraT = Camera.main.transform;
     }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            modelAnim.SetTrigger("Jab");
+        else if (Input.GetMouseButtonDown(1))
+            modelAnim.SetTrigger("Hook");
+        else if (Input.GetKeyDown(KeyCode.Space))
+            modelAnim.SetTrigger("Block");
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
-        movementVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * force;
-        if (movementVector != Vector3.zero)
+        if (falling)
+            return;
+        movementVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        if (movementVector != Vector3.zero && movementVector.z >= 0)
         {
             float targetRotation = Mathf.Atan2(movementVector.x, movementVector.y) * Mathf.Rad2Deg + cameraT.eulerAngles.y;
             transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSmoothTime);
+            modelAnim.SetBool("Run", true);
         }
-        transform.Translate(movementVector * Time.fixedDeltaTime * force, Space.Self);
-        //rigidbodyMover.AddForce(movementVector);
+        else if (movementVector.z < 0)
+        {
+            modelAnim.SetBool("Run", true);
+            transform.LookAt(cameraT.position);
+            transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+        }
+        else
+        {
+            modelAnim.SetBool("Run", false);
+            rigidbodyMover.velocity = Vector3.zero;
+        }
+
+        if (movementVector != Vector3.zero)
+        {
+            if (rigidbodyMover.velocity.magnitude < maxVelocity)
+                //rigidbodyMover.AddForce(transform.forward * force);
+                rigidbodyMover.velocity = transform.forward * force;
+        }
+        //else
+        //    Debug.Log("Reached max velocity");
         //transform.forward = testRb.velocity.normalized;
         //transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
         //if(movementVector != Vector2.zero)
         //    transform.forward = movementVector;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Floor"))
+        {
+            stringy.SetActive(false);
+            modelAnim.SetBool("Fall", true);
+            modelAnim.SetBool("Run", false);
+            rigidbodyMover.AddTorque(Vector3.forward * 10000);
+            falling = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Floor"))
+        {
+            stringy.transform.position = headTr.position;
+            stringy.SetActive(true);
+            modelAnim.SetBool("Fall", false);
+            falling = false;
+        }
     }
 }
---
+++ Assets/Scripts/CharacterUpright.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterUpright : MonoBehaviour
+{
+    new protected Rigidbody rigidbody;
+    public bool keepUpright = true;
+    public float uprightForce = 10f;
+    public float uprightOffset = 1.45f;
+    public float additionalUpwardForce = 10f;
+    public float dampenAngularForce = 0;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.maxAngularVelocity = 40;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(keepUpright)
+        {
+            rigidbody.AddForceAtPosition(new Vector3(0, (uprightForce + additionalUpwardForce), 0),
+                transform.position + transform.TransformPoint(new Vector3(0, uprightOffset, 0)), ForceMode.Force);
+
+            rigidbody.AddForceAtPosition(new Vector3(0, -uprightForce, 0),
+                transform.position + transform.TransformPoint(new Vector3(0, -uprightOffset, 0)), ForceMode.Force);
+
+        }
+
+        if(dampenAngularForce > 0)
+        {
+            rigidbody.angularVelocity *= (1 - Time.deltaTime * dampenAngularForce);
+        }
+    }
+}
